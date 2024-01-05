aac106e5f17c37c4f5ed139fe006d004a891688c
HHH <shion198823@gmail.com>
2024-01-05 19:51:22 +0800

    2024-01-05 revised pt.1

--- abbreviate_two_word_name.py
+++ abbreviate_two_word_name.py
@@ -1,14 +1,5 @@
-first_name = input("請輸入英文名：")
-last_name = input("請輸入英文姓：")
+def abbrev_name(name):
+    names = name.split()
+    return f"{names[0][0]}.{names[1][0]}".upper()
 
-def abbrev_name(first_name, last_name):
 
-    #目標：取姓跟名的第一個字（大寫）組合成縮寫
-    #list + capitalize + join
-    firstname_list = list(first_name.capitalize())
-    lastname_list = list(last_name.capitalize())
-    print(f"{firstname_list[0]}.{lastname_list[0]}")
-
-    pass
-
-abbrev_name(first_name, last_name)
